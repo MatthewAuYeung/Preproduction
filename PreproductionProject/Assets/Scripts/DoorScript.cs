ebe45ecf7093363a168f4a7324f630aa6dfb142b
Zijia Li <allenlizijia@outlook.com>
2019-11-05 17:18:44 -0800

    Create door and key prefab

---
+++ PreproductionProject/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public List<KeyScript> Keys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Keys.RemoveAll(item => item == null);
+        if (Input.GetKey(KeyCode.A) )
+        {
+            if (Keys.Count == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
