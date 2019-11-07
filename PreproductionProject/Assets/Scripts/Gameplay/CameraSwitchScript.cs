a9b977ad19d22f5d99643cd26d4a43b648dc7783
Chun Hei MatthewAu Yeung <1810192@AIV.EDMC.STU>
2019-11-07 11:11:13 -0800

    Changed CameraSwitch from Setting cameras active to moving the main camera's transform

--- PreproductionProject/Assets/Scripts/Gameplay/CameraSwitchScript.cs
+++ PreproductionProject/Assets/Scripts/Gameplay/CameraSwitchScript.cs
@@ -6,33 +6,47 @@ public class CameraSwitchScript : MonoBehaviour
 {
     public PlayerScript Player;
 
-    [Header("Cameras")]
-    public GameObject CameraOne;
-    public GameObject CameraTwo;
+    [Header("MainCamera")]
+    public GameObject MainCamera;
 
-    AudioListener cameraOneLis;
-    AudioListener cameraTwoLis;
+    [Header("CameraOne Transform")]
+    public float XPos1;
+    public float YPos1;
+    public float ZPos1;
+
+    public float XRotate1;
+    public float YRotate1;
+    public float ZRotate1;
+
+    [Header("CameraTwo Transform")]
+    public float XPos2;
+    public float YPos2;
+    public float ZPos2;
+
+    public float XRotate2;
+    public float YRotate2;
+    public float ZRotate2;
 
     private int _cameraCount;
-    // Start is called before the first frame update
-    void Start()
+
+    private Vector3 CamOnePos;
+    private Vector3 CamOneRotate;
+
+    private Vector3 CamTwoPos;
+    private Vector3 CamTwoRotate;
+
+    private void Awake()
     {
-        cameraOneLis = CameraOne.GetComponent<AudioListener>();
-        cameraTwoLis = CameraTwo.GetComponent<AudioListener>();
+        CamOnePos = new Vector3(0.0f, 1.0f, -10.0f);
+        CamOneRotate = new Vector3(0.0f, 0.0f, 0.0f);
+
+        CamTwoPos = new Vector3(-7.97f, 1.0f, 8.41f);
+        CamTwoRotate = new Vector3(0.0f, 90.0f, 0.0f);
 
         _cameraCount = 1;
-        CameraTwo.SetActive(false);
-        cameraTwoLis.enabled = false;
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PlayerTag"))
         {
@@ -40,22 +54,15 @@ public class CameraSwitchScript : MonoBehaviour
             {
                 case 1:
                     {
-                        CameraOne.SetActive(false);
-                        cameraOneLis.enabled = false;
-
-                        CameraTwo.SetActive(true);
-                        cameraTwoLis.enabled = true;
+                        MainCamera.transform.position = CamTwoPos;
+                        MainCamera.transform.rotation = Quaternion.Euler(CamTwoRotate);
                         _cameraCount = 2;
                         break;
                     }
                 case 2:
                     {
-                        CameraOne.SetActive(true);
-                        cameraOneLis.enabled = true;
-
-                        CameraTwo.SetActive(false);
-                        cameraTwoLis.enabled = false;
-
+                        MainCamera.transform.position = CamOnePos;
+                        MainCamera.transform.rotation = Quaternion.Euler(CamOneRotate);
                         _cameraCount = 1;
                         break;
                     }
