13cd8a610aefa3c70996b74ec25a7a65d7f81d1d
Zijia Li <allenlizijia@outlook.com>
2019-11-07 11:12:01 -0800

    Ini DoorPrefab Script and door active and inactive

---
+++ PreproductionProject/Assets/Scripts/DoorPrefabScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPrefabScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<KeyScript> Keys;
+    private DoorScript _door;
+    private void Awake()
+    {
+        _door = GetComponentInChildren<DoorScript>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Keys.RemoveAll(item => item == null);
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Keys.Count == 0)
+            {
+                _door.OpenDoor();
+
+            }
+        }
+        if (!_door.IsDoorActive())
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                _door.CloseDoor();
+            }
+        }
+
+    }
+}
--- PreproductionProject/Assets/Scripts/DoorScript.cs
+++ PreproductionProject/Assets/Scripts/DoorScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    public List<KeyScript> Keys;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +14,26 @@ public class DoorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Keys.RemoveAll(item => item == null);
-        if (Input.GetKey(KeyCode.A) )
-        {
-            if (Keys.Count == 0)
-            {
-                Destroy(gameObject);
-            }
-        }
+       
+
+
+       
+
+    }
+
+    public void OpenDoor()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void CloseDoor()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public bool IsDoorActive()
+    {
+        return gameObject.activeSelf;
     }
 }
 
