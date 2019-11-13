afb59aeafd5957ea78dd5f91610f95dc23026f73
Chun Hei MatthewAu Yeung <1810192@AIV.EDMC.STU>
2019-11-12 17:32:44 -0800

    Modified DoorPrefab and KeyPreab. Door can now open when the player is in a certain range and key is now a pick up.

--- PreproductionProject/Assets/Scripts/DoorPrefabScript.cs
+++ PreproductionProject/Assets/Scripts/DoorPrefabScript.cs
@@ -5,36 +5,53 @@ using UnityEngine;
 public class DoorPrefabScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float Range;
     public List<KeyScript> Keys;
+
     private DoorScript _door;
+    private GameObject _player;
+    private float _distanceToPlayer;
     private void Awake()
     {
         _door = GetComponentInChildren<DoorScript>();
+        _player = GameObject.FindGameObjectWithTag("PlayerTag");
     }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Keys.RemoveAll(item => item == null);
-        if (Input.GetKey(KeyCode.A))
-        {
-            if (Keys.Count == 0)
-            {
-                _door.OpenDoor();
+        _distanceToPlayer = Vector3.Distance(transform.position, _player.transform.position);
 
+        Keys.RemoveAll(item => item == null);
+
+        if(_distanceToPlayer <= Range)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (Keys.Count == 0)
+                {
+                    _door.OpenDoor();
+
+                }
             }
         }
         if (!_door.IsDoorActive())
         {
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.V))
             {
                 _door.CloseDoor();
             }
         }
-
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+
 }
--- PreproductionProject/Assets/Scripts/KeyScript.cs
+++ PreproductionProject/Assets/Scripts/KeyScript.cs
@@ -13,7 +13,16 @@ public class KeyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Collided with " + other.gameObject.name);
+        if (other.gameObject.CompareTag("PlayerTag"))
         {
             Destroy(gameObject);
         }
