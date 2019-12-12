3a8614baeb94056afe94ad36261380b93cd255c0
JohnWu <wusiyuan54111@gmail.com>
2019-12-12 13:23:00 -0800

    [John] - import my openNotes prefabs

---
+++ PreproductionProject/Assets/Scripts/John_UI/TriggerNotes/ShowUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+    public GameObject uiObject;
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void OnTriggerEnter (Collider player)
+    {
+        Debug.Log("Hit");
+        if(player.gameObject.tag == "PlayerTag")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()//wait for secs to read text
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+}
