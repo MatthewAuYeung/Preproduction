9b31b3c4478aeff8c90fdcd58b9e41fcc79a603b
JohnWu <wusiyuan54111@gmail.com>
2019-12-16 23:15:00 -0800

    [JohnWu_UI] ADDED: - add the range of player move to check zone and press "o" to show "open notes" button - add click "open notes" button to show "close notes" button TODO: - need to implement swicht between "open notes" and "close notes" button, and once click "close notes" button, the notes will hide

--- PreproductionProject/Assets/Scripts/John_UI/TriggerNotes/NotesOpener.cs
+++ PreproductionProject/Assets/Scripts/John_UI/TriggerNotes/NotesOpener.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class NotesOpener : MonoBehaviour
 {
     public GameObject Panel;
+    public GameObject CloseButton;
 
     public void OpenNotes()
     {
-        if (Panel != null)
+        if (Panel != null && CloseButton != null)
         {
             bool isActive = Panel.activeSelf;
 
             Panel.SetActive(!isActive);
+            CloseButton.SetActive(!isActive);
         }
     }
 }
--- PreproductionProject/Assets/Scripts/John_UI/TriggerNotes/ShowUI.cs
+++ PreproductionProject/Assets/Scripts/John_UI/TriggerNotes/ShowUI.cs
@@ -4,25 +4,66 @@ using UnityEngine;
 
 public class ShowUI : MonoBehaviour
 {
+    public float Range;
+
+    private GameObject _player;
+    private float _distanceToPlayer;
     public GameObject uiObject;
+
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("PlayerTag");
+    }
     void Start()
     {
         uiObject.SetActive(false);
     }
-    // Update is called once per frame
-    void OnTriggerEnter (Collider player)
+
+    private void Update()
     {
-        Debug.Log("Hit");
-        if(player.gameObject.tag == "PlayerTag")
+        bool b = false;
+        _distanceToPlayer = Vector3.Distance(transform.position, _player.transform.position);
+        if (_distanceToPlayer <= Range)
         {
-            uiObject.SetActive(true);
-            StartCoroutine("WaitForSec");
+            b = true;
+        }
+        if(b)
+        {
+            if (Input.GetKey(KeyCode.O))
+            {
+                uiObject.SetActive(true);
+                ShowMouseCursor();
+            }
         }
     }
+
+    void ShowMouseCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    // Update is called once per frame
+    //void OnTriggerEnter (Collider player)
+    //{
+    //    Debug.Log("Hit");
+    //    if(player.gameObject.tag == "PlayerTag")
+    //    {
+    //        uiObject.SetActive(true);
+    //        StartCoroutine("WaitForSec");
+    //    }
+    //}
     IEnumerator WaitForSec()//wait for secs to read text
     {
         yield return new WaitForSeconds(5);
         Destroy(uiObject);
         Destroy(gameObject);
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+
+
 }
