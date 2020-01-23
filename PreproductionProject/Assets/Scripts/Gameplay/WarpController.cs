eb56710e2e2f417b2b12a6b7ad4a7f3f8ceeb0d1
Matthew <galaxys2.matthew0621@gmail.com>
2020-01-22 20:57:51 -0800

    [Matthew] --- Finished implementing not warpable walls.

--- PreproductionProject/Assets/Scripts/Gameplay/WarpController.cs
+++ PreproductionProject/Assets/Scripts/Gameplay/WarpController.cs
@@ -10,23 +10,13 @@ public class WarpController : MonoBehaviour
     [SerializeField]
     private float warpDuration = 1.0f;
 
+    [SerializeField]
+    private float warpOffset = 0.5f;
+
     public GameObject ybot;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Vector3 warpDir = Camera.main.transform.forward;
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position + transform.up, warpDir.normalized, out hit, warpRange))
-            {
-                if (hit.collider.gameObject.CompareTag("NotWarpable"))
-                {
-                    Debug.Log(hit.transform.name); ;
-                }
-            }
-        }
-
         if (Input.GetKeyDown(KeyCode.T))
         {
             FreeWarp();
@@ -38,16 +28,28 @@ public class WarpController : MonoBehaviour
         Vector3 warpDir = Camera.main.transform.forward;
         warpDir.y = 0.0f;
         RaycastHit hit;
+        ShowBody(false);
         if (Physics.Raycast(transform.position + transform.up, warpDir.normalized, out hit, warpRange))
         {
             var hitObj = hit.collider.gameObject;
             if (hitObj.CompareTag("NotWarpable"))
             {
-                //hitObj.transform.position
-                transform.DOMove(hitObj.transform.position, warpDuration).OnComplete(() => EndWarp());
+                Vector2 PlayerPos = new Vector2(transform.position.x, transform.position.z);
+                Vector2 ObjPos = new Vector2(hitObj.transform.position.x, hitObj.transform.position.z);
+                Vector2 OffsetVec = PlayerPos - ObjPos;
+                Vector2 OffsetDir = OffsetVec.normalized;
+                //Vector2 OffsetDir = vector3.normalized;
+                float offset = hitObj.transform.localScale.z;
+                Vector3 newWarpPos = hitObj.transform.position;
+                newWarpPos.x += OffsetDir.x * warpOffset;
+                newWarpPos.z += OffsetDir.y * warpOffset;
+
+                newWarpPos.y = 0.0f;
+                
+                transform.DOMove(newWarpPos, warpDuration).OnComplete(() => EndWarp());
+                return;
             }
         }
-        ShowBody(false);
         transform.DOMove(transform.position + warpDir.normalized * warpRange, warpDuration).OnComplete(() => EndWarp());
     }
 
