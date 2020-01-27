ffa4929c6381c90f72ac4d1b2ce6c7b3c00e6b50
JohnWu <wusiyuan54111@gmail.com>
2020-01-27 14:57:05 -0800

    Fixed the combo system

--- PreproductionProject/Assets/Scripts/John_Scripts/ComboController.cs
+++ PreproductionProject/Assets/Scripts/John_Scripts/ComboController.cs
@@ -7,15 +7,16 @@ public class ComboController : MonoBehaviour
     Animator animator;
     Rigidbody rigidBody;
 
-    int noOfClicks;                 // Determines which animation will play
+    int attackIndex = 0;                 // Determines which animation will play
     bool canClick;                  // Locks ability to click during animation event
+    private const int totalAttacks = 3;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
 
-        noOfClicks = 0;             // numbers of clicks
+        attackIndex = 0;             // numbers of clicks
         canClick = true;
     }
 
@@ -24,54 +25,19 @@ public class ComboController : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            ComboStarter();
+            Attack();
         }
     }
 
-    void ComboStarter()
+    void Attack()
     {
-        if (canClick)
-        {
-            noOfClicks++;
-        }
+        string attackTrigger = "Attack" + (attackIndex+1).ToString();
 
-        if (noOfClicks == 1)
-        {
-            animator.SetInteger("animation", 11);
-        }
-    }
+        animator.SetTrigger(attackTrigger);
 
-    public void ComboCheck()
-    {
-        canClick = false;
-
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("State_11") && noOfClicks == 1)
-        {// If the first animation is still playing and only 1 click has happended, return to idle
-            animator.SetInteger("animation", 1);
-            canClick = true;
-            noOfClicks = 0;
-        }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("State_11") && noOfClicks == 2)
-        {// If the first animation is still playing and at least 2 clicks has happended, continue the combo
-            animator.SetInteger("animation", 13);
-            canClick = true;
-        }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("State_12") && noOfClicks == 2)
-        {// If the second animation is still playing and only 2 clicks has happended, return to idle
-            animator.SetInteger("animation", 1);
-            canClick = true;
-            noOfClicks = 0;
-        }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("State_12") && noOfClicks == 3)
-        {// If the second animation is still playing and at least 3 clicks has happended, continue the combo
-            animator.SetInteger("animation", 13);
-            canClick = true;
-        }
-        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("State_13") )
-        {// Since this is the third and last animation, return to idle
-            animator.SetInteger("animation", 1);
-            canClick = true;
-            noOfClicks = 0;
-        }
+        if (attackIndex < totalAttacks-1)
+            attackIndex++;
+        else
+            attackIndex = 0;
     }
 }
--- PreproductionProject/Assets/Scripts/John_Scripts/WeaponManager.cs
+++ PreproductionProject/Assets/Scripts/John_Scripts/WeaponManager.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
-    
+    public Collider weaponColldier;
+
+    private void Start()
+    {
+        weaponColldier.enabled = false;
+    }
+
+    public void AttackStart()
+    {
+        weaponColldier.enabled = true;
+    }
+
+    public void AttackStop()
+    {
+        weaponColldier.enabled = false;
+    }
 }
