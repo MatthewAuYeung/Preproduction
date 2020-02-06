502dc057d29dba88214fa88e8f9fa9e09a882a13
Matthew <galaxys2.matthew0621@gmail.com>
2020-02-05 16:27:17 -0800

    [Matthew] --- Added cooldown for warp. Added mana to player. Hookded up mana bar to the mana. Warping now uses mana. WIP: Mana regenartion.

--- PreproductionProject/Assets/Scripts/Gameplay/NewPlayerScript.cs
+++ PreproductionProject/Assets/Scripts/Gameplay/NewPlayerScript.cs
@@ -5,28 +5,52 @@ using UnityEngine;
 public class NewPlayerScript : MonoBehaviour
 {
     public SimpleHealthBar healthBar;
+    public SimpleHealthBar manaBar;
     public float AttckDamage;
     public int hitcount = 0;
     [SerializeField]
     private float health;
     [SerializeField]
     private float maxHealth;
+    [SerializeField]
+    private float mana;
+    [SerializeField]
+    private float maxMana;
+    [SerializeField]
+    float warpCooldown;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        health = maxHealth;
+        mana = maxMana;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(mana < maxMana)
+        {
+            mana += 1.0f;
+        }
         healthBar.UpdateBar(health, maxHealth);
+        manaBar.UpdateBar(mana, maxMana);
     }
 
+    //IEnumerator RegenerateOverTime()
+    //{
+
+    //}
     public void TakeDamage(float damage)
     {
         health -= damage;
+    }
+
+    public void UseMana(float amount)
+    {
+        mana -= amount;
+    }
+
+    public float GetWarpCooldown()
+    {
+        return warpCooldown;
     }
 }
