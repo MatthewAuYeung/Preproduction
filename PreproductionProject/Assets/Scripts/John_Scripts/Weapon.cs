008e7ef6319b15d3759745732d4a5820aa1bba3f
Matthew <galaxys2.matthew0621@gmail.com>
2020-02-29 12:58:19 -0800

    [Matthew] - Merged John's attack animation movements to the branch

---
+++ PreproductionProject/Assets/Scripts/John_Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Weapon : MonoBehaviour
+{
+    NewPlayerScript playerScript;
+    private float attackDmg;
+    
+    private void Awake()
+    {
+        playerScript = FindObjectOfType<NewPlayerScript>();
+        attackDmg = playerScript.AttckDamage;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EnemyTag"))
+        {
+            EnemyScript enemy = other.GetComponent<EnemyScript>();
+            enemy.TakeDamage(attackDmg);
+            playerScript.IncrementHitPoint();
+        }
+    }
+}
+
+
